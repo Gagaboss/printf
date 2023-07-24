7d9111703b99cbfa0209598300c7a410560afd31
Zerish Lamptey <135115987+Lampteyzerish@users.noreply.github.com>
2023-07-24 05:57:47 -0700

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Calculate the width for printing
  * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
+ * @i: List of arguments to be print.
  * @list: list of arguments.
  *
  * Return: width.
